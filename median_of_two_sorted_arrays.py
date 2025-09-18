75a6b44e4a00b9bf87ad8db7e9781876d8c66096
David <davidmartin1737@gmail.com>
2025-09-18 14:22:51 -0700

    clean up and comments

--- median_of_two_sorted_arrays.py
+++ median_of_two_sorted_arrays.py
@@ -8,15 +8,16 @@ class Solution:
         length = len(nums)
         if length % 2 == 0:
             middle = length//2
-            median = (nums[middle] + nums[(middle-1)])/2 
+            median = (nums[middle] + nums[(middle-1)])/2
         else: 
             target = ((length + 1)//2)-1
             median = nums[target]
 
         return median
+
 # The function find_median_sorted_arrays takes two sorted arrays (nums1 and nums2) as input and returns the median of the combined sorted array.
-# The algorithm follows these steps:          
+# The algorithm follows these steps:
 # 1. Merge the two arrays into one sorted array.
 # 2. Find the length of the merged array.
 # 3. If the length is odd, return the middle element.
-# 4. If the length is even, return the average of the two middle elements.  
+# 4. If the length is even, return the average of the two middle elements.
