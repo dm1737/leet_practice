9970eee72deb80ca862403c791301387e09174af
dm1737 <davidmartin1737@gmail.com>
2025-10-28 17:19:28 -0500

    missing num

---
+++ missing_num.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i, val in enumerate(nums):
+            if i != val:
+                return i
+        return len(nums)
