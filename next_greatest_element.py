0a541d8720c4ce31911266296a38fa6d853a4389
dm1737 <davidmartin1737@gmail.com>
2025-11-26 11:31:15 -0600

    next greatest elements

---
+++ next_greatest_element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        mapper = {}
+        
+        for num in nums2:
+            while stack and stack[-1] < num:
+                mapper[stack.pop()] = num
+            
+            stack.append(num)
+            
+        return [mapper.get(i, -1) for i in nums1]
