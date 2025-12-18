7297d102ab08537e57ddceded1ebcf8e0dc6a043
dm1737 <davidmartin1737@gmail.com>
2025-12-18 13:24:31 -0600

    zig zag tree

---
+++ zig_zag_tree.py
@@ -0,0 +1,37 @@
+from collections import deque, List
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        queue = deque([root])
+        ans = []
+        rev_flag = False
+        
+        while queue:
+            temp = []
+            row_len = len(queue)
+            for _ in range(row_len):
+                node = queue.popleft()
+                temp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            if rev_flag is True:
+                temp.reverse()
+                ans.append(temp)
+                rev_flag = False
+            else:
+                ans.append(temp)
+                rev_flag = True
+        
+        return ans
